0579cbafe126badc9e56d88a4c05d98776426ff8
Kristin Hansen <maagenator@gmail.com>
2016-10-24 17:19:28 +0200

    qrcamera about to be implemented

---
+++ app/src/main/java/mau/resturantapp/test/QRCamera.java
@@ -0,0 +1,19 @@
+package mau.resturantapp.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class QRCamera extends Fragment {
+    private View rod;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //rod
+        return rod;
+    }
+}
