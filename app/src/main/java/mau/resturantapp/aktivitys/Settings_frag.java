77810a76aaecca19215a17aab7f37c915f451d00
Anwar <anwar_choukair@live.dk>
2016-10-24 17:41:44 +0200

    alle knapperne i menu virker

--- app/src/main/java/mau/resturantapp/aktivitys/Settings_frag.java
+++ app/src/main/java/mau/resturantapp/aktivitys/Settings_frag.java
@@ -1,8 +1,26 @@
 package mau.resturantapp.aktivitys;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mau.resturantapp.R;
+
 /**
  * Created by anwar on 10/16/16.
  */
 
-public class Settings_frag {
+public class Settings_frag extends Fragment {
+    private View rod;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rod = inflater.inflate(R.layout.settings_frag, container, false);
+
+
+        return rod;
+    }
 }
