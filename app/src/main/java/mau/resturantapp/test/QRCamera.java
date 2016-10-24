df89908a661f7405aeb61fda42d0d8377265f7ce
Kristin Hansen <maagenator@gmail.com>
2016-10-24 17:30:15 +0200

    !!

--- app/src/main/java/mau/resturantapp/test/QRCamera.java
+++ app/src/main/java/mau/resturantapp/test/QRCamera.java
@@ -7,13 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import mau.resturantapp.R;
+
 public class QRCamera extends Fragment {
     private View rod;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //rod
+        rod = inflater.inflate(R.layout.qr_camera_test, container, false);
+
+
+
         return rod;
     }
 }
