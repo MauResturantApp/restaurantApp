e0d6e4483c2587ee293106bbca4ceaecbe493600
Kristin Hansen <maagenator@gmail.com>
2016-10-24 15:56:59 +0200

    meh...

--- app/src/main/java/mau/resturantapp/aktivitys/FindWay_frag.java
+++ app/src/main/java/mau/resturantapp/aktivitys/FindWay_frag.java
@@ -11,7 +11,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
