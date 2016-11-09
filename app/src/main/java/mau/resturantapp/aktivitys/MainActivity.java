d9b1737f20ba9692f8d8fd64e6b8a60fbc661374
Anwar <anwar_choukair@live.dk>
2016-11-09 22:08:05 +0100

    problemer med lorte bottom bar er fixede, der er en side menu nu.

--- app/src/main/java/mau/resturantapp/aktivitys/MainActivity.java
+++ app/src/main/java/mau/resturantapp/aktivitys/MainActivity.java
@@ -1,79 +1,64 @@
 package mau.resturantapp.aktivitys;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MotionEvent;
+
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnMenuTabSelectedListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
 import mau.resturantapp.R;
-import mau.resturantapp.data.MenuItem;
-import mau.resturantapp.data.appData;
 import mau.resturantapp.events.OnSuccesfullLogInEvent;
-import mau.resturantapp.test.QRCamera;
-import mau.resturantapp.test.QRTest;
+
 
 import static android.support.design.widget.BottomSheetBehavior.*;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener, OnMenuTabSelectedListener {
+public class MainActivity extends AppCompatActivity implements OnClickListener, OnTabSelectListener {
 
     //private Toolbar mainTollbar;
     private FloatingActionButton actBtn;
     private BottomSheetBehavior bottomSheetBehavior;
     private BottomBar bottomBar;
+    private ImageButton drawerButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hovedakt_akt);
-        //mainTollbar = (Toolbar) findViewById(R.id.mainToolbar);
-       // setSupportActionBar(mainTollbar);
         EventBus.getDefault().register(this);
-       // getSupportActionBar().setTitle(null);
         showHomeScreen();
 
-        bottomBar = BottomBar.attach(this,savedInstanceState);
-        bottomBar.setItemsFromMenu(R.menu.bottom_menu,this);
-
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar_main);
+        drawerButton = (ImageButton) findViewById(R.id.btn_sidemenu);
         FrameLayout sheet = (FrameLayout) findViewById(R.id.frameLayout_bottomSheet);
         bottomSheetBehavior = from(sheet);
+        drawerButton.setOnClickListener(this);
 
+        bottomBar.setOnTabSelectListener(this);
 
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-        float dp = 46f;
-        float fpixels = metrics.density * dp;
-        int pixels = (int) (fpixls + 0.5f);
-
-        bottomSheetBehavior.setPeekHeight(pixels);
+        int dp = DPtoPixels(46f);
+        bottomSheetBehavior.setPeekHeight(dp);
         bottomSheetBehavior.setHideable(true);
 
         bottomSheetBehavior.setState(STATE_COLLAPSED);
 
         bottomBar.setElevation(4);
-
-        bottomBar.setActiveTabColor("#e00404");
-        bottomBar.setBackgroundColor(Color.rgb(157,219,50));
 
         actBtn = (FloatingActionButton) findViewById(R.id.floatActBtn_cartContent);
 
@@ -93,6 +78,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
             else
                 bottomSheetBehavior.setState(STATE_EXPANDED);
 
+        }
+        if(v == drawerButton){
+            // TODO: 11/9/16
         }
 
     }
@@ -193,33 +181,49 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
         ft.replace(R.id.mainFrameFrag, frag).commit();
     }
 
+    private int DPtoPixels(float DP){
+       int pixels = 0;
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float dp = DP;
+        float fpixels = metrics.density * dp;
+        pixels = (int) (fpixels + 0.5f);
+        return pixels;
+    }
+
 
     @Override
-    public void onMenuItemSelected(@IdRes int menuItemId) {
+    public void onTabSelected(@IdRes int tabId) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         Fragment frag;
 
-        switch (menuItemId){
-            case R.id.menuItem_food:
+        Log.d("tab slected", "clicked " + tabId + " --- " + R.id.botbar_food);
+
+        switch (tabId){
+            case R.id.botbar_food:
                 frag = new MenuTabs_frag();
                 ft.addToBackStack(null);
                 ft.replace(R.id.mainFrameFrag, frag).commit();
 
                 break;
 
-            case R.id.menuItem_home:
+            case R.id.botbar_home:
                 frag = new Home_frag();
                 ft.addToBackStack(null);
                 ft.replace(R.id.mainFrameFrag, frag).commit();
+
                 break;
 
-            case R.id.menuItem_contact:
+            case R.id.botbar_contact:
                 frag = new Contact_frag();
                 ft.addToBackStack(null);
                 ft.replace(R.id.mainFrameFrag, frag).commit();
+                Log.d("tab slected", "contact");
+
                 break;
             default:
                 break;
         }
+
     }
 }
