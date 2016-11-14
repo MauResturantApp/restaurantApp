ed2a6bf251a754f257577a418a15adf82682c7ef
Yoouughurt <Yoouughurt>
2016-11-14 01:37:30 +0100

    admin check

--- app/src/main/java/mau/resturantapp/data/appData.java
+++ app/src/main/java/mau/resturantapp/data/appData.java
@@ -14,7 +14,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -146,11 +150,27 @@ public class appData extends Application{
         OnSuccesfullLogInEvent event = new OnSuccesfullLogInEvent();
         EventBus.getDefault().post(event);
         String name = firebaseAuth.getCurrentUser().getDisplayName();
-        String email = firebaseAuth.getCurrentUser().getDisplayName();
+        String email = firebaseAuth.getCurrentUser().getEmail();
         currentUser = new LoggedInUser(name,email,0);
-
+        isAdmin();
     }
 
+    private static void isAdmin() {
+        DatabaseReference ref = firebaseDatabase.getReference("permissions/" + getUID());
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    currentUser.setAdmin(true);
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Admin check error", databaseError.getMessage());
+            }
+        });
+
+    }
 
 }
