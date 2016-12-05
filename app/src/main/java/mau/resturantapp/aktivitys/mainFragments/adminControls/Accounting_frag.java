acf238395f3e3cce8d51eb2ada1f877542eb34dd
AnwarC <anwar_choukair@live.dk>
2016-12-05 21:19:05 +0100

    layouts er også sat op nu, så i kan fyre løs.

--- app/src/main/java/mau/resturantapp/aktivitys/mainFragments/adminControls/Accounting_frag.java
+++ app/src/main/java/mau/resturantapp/aktivitys/mainFragments/adminControls/Accounting_frag.java
@@ -1,10 +1,24 @@
 package mau.resturantapp.aktivitys.mainFragments.adminControls;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mau.resturantapp.R;
 
 /**
  * Created by AnwarC on 05/12/2016.
  */
 
 public class Accounting_frag extends Fragment {
+    private View root;
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+       root = inflater.inflate(R.layout.amdcontrol_accounting,container,false);
+
+        return root;
+    }
 }
--- app/src/main/java/mau/resturantapp/aktivitys/mainFragments/adminControls/ShopSettings_frag.java
+++ app/src/main/java/mau/resturantapp/aktivitys/mainFragments/adminControls/ShopSettings_frag.java
@@ -1,10 +1,25 @@
 package mau.resturantapp.aktivitys.mainFragments.adminControls;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mau.resturantapp.R;
 
 /**
  * Created by AnwarC on 05/12/2016.
  */
 
 public class ShopSettings_frag extends Fragment {
+    private View root;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       root = inflater.inflate(R.layout.amdcontrol_shopsettings,container,false);
+
+
+        return root;
+    }
 }
