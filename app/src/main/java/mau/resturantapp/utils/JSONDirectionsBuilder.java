46b7e45151bd02bc8f89413101322227bac40daf
Kristin Hansen <maagenator@gmail.com>
2016-12-11 14:53:35 +0100

    another fix...

---
+++ app/src/main/java/mau/resturantapp/utils/JSONDirectionsBuilder.java
@@ -0,0 +1,12 @@
+package mau.resturantapp.utils;
+
+import org.json.JSONArray;
+
+import java.util.List;
+
+public class JSONDirectionsBuilder {
+    public static List<String> parse() {
+
+        return null;
+    }
+}
