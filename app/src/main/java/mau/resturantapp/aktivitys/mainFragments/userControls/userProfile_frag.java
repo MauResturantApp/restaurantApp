8d61dea4058a0169b7c8e6b8b6e35f308b12e3ca
Yoouughurt <Yoouughurt>
2016-12-19 04:28:01 +0100

    FCM in progress

--- app/src/main/java/mau/resturantapp/aktivitys/mainFragments/userControls/userProfile_frag.java
+++ app/src/main/java/mau/resturantapp/aktivitys/mainFragments/userControls/userProfile_frag.java
@@ -12,14 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import mau.resturantapp.R;
 import mau.resturantapp.data.UserProfile;
@@ -101,47 +99,24 @@ public class userProfile_frag extends Fragment implements View.OnClickListener {
 
         @Override
         protected Void doInBackground(Void... params) {
-            HttpURLConnection connection = null;
+            byte[] data = token.getBytes();
+            InetAddress ipAddress = null;
             try {
-                //Create connection
-
-                URL url = new URL("http://:9000");
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Content-Type",
-                        "application/x-www-form-urlencoded");
-
-                connection.setRequestProperty("Content-Length",
-                        Integer.toString(token.getBytes().length));
-                connection.setRequestProperty("Content-Language", "en-US");
-
-                connection.setUseCaches(false);
-                connection.setDoOutput(true);
-
-                //Send request
-                DataOutputStream wr = new DataOutputStream (
-                        connection.getOutputStream());
-                wr.writeBytes(token);
-                wr.close();
-
-                //Get Response
-                InputStream is = connection.getInputStream();
-                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-                StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-                String line;
-                while ((line = rd.readLine()) != null) {
-                    response.append(line);
-                    response.append('\r');
-                }
-                rd.close();
-                Log.d("TOKEN TO SERVER", response.toString());
-            } catch (Exception e) {
+                ipAddress = InetAddress.getByName("");
+            } catch (UnknownHostException e) {
                 e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
             }
+            DatagramPacket packet = new DatagramPacket(data, data.length, ipAddress, 9000);
+            DatagramSocket socket = null;
+            try {
+                socket = new DatagramSocket();
+                socket.send(packet);
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.d("Packet send", packet.toString());
             return null;
         }
     }
